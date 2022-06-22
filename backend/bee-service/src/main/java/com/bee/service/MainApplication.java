c9a6d3674161d6cc783030e2b21899be01eea085
guofan <fan.guo@telecwin.com>
2022-06-22 12:14:20 +0800

    初始化项目

---
+++ backend/bee-service/src/main/java/com/bee/service/MainApplication.java
@@ -0,0 +1,18 @@
+package com.bee.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主函数入库
+ *
+ * @author guofan
+ * @date 2022/6/22
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+//        System.out.println("================");
+    }
+}
