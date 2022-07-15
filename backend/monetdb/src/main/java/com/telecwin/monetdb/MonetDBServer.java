d5e4312104c044b76c4779ed5a0fe84a126cb1d6
bo.yang <bo.yang@telecwin.com>
2022-07-15 15:20:12 +0800

    开发控制 MonetDB exe程序的类

--- backend/monetdb/src/main/java/com/telecwin/monetdb/MonetDBServer.java
+++ backend/monetdb/src/main/java/com/telecwin/monetdb/MonetDBServer.java
@@ -1,7 +1,12 @@
 package com.telecwin.monetdb;
 
-import java.io.File;
-import java.io.IOException;
+import nl.cwi.monetdb.mcl.net.MapiSocket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -33,6 +38,8 @@ public class MonetDBServer {
      */
     private File dbPath;
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     /**
      * @param exePath 服务程序 mserver5.exe 的路径
      * @param dbPath  数据库路径
@@ -53,15 +60,10 @@ public class MonetDBServer {
         // 2. 设置 dbpath 路径
         // 3. 启动进程，记录终端输出到日志文件
 
-        // 1.  扩展 PATH 环境变量，加入 monetDB 的 bin/ 和 lib/ 目录
-        String path = System.getenv("PATH");
-        File bin = new File(baseDir, "bin");
-        File lib = new File(baseDir, "lib/monetdb5");
-        path = String.format("%s;%s;%s", bin, lib, path);
-        System.getenv().put("PATH", path);
+        // 先停止旧进程
+        stopServer();
 
-        // 2. 设置 dbpath 路径
-        // 3. 启动进程
+        // 启动进程
         // "%MONETDB%\bin\mserver5.exe" --set "prefix=%MONETDB%" --set %MONETDBPYTHONUDF% --set "exec_prefix=%MONETDB%" --set mal_for_all=yes %MONETDBFARM% %*
         ProcessBuilder procBuilder = new ProcessBuilder(this.exePath.toString(),
                 // 末尾不能有 "/" 符号
@@ -71,11 +73,53 @@ public class MonetDBServer {
                 "--set", "mal_for_all=yes",
                 "--dbpath=" + this.dbPath
         );
+        // 修改创建子进程时的环境变量
+        changeEnvironmentVariables(procBuilder.environment());
+
+        Process process = procBuilder.start();
+        InputStream inputStream = process.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            logger.debug("mserver5.exe >> {}", line);
+        }
         return null;
     }
 
-    public void stopServer() {
+    /**
+     * 修改环境变量字典
+     *
+     * @param environment 环境变量字典
+     */
+    private void changeEnvironmentVariables(Map<String, String> environment) {
+        String path = environment.get("PATH");
+        File bin = new File(baseDir, "bin");
+        File lib = new File(baseDir, "lib/monetdb5");
+        path = String.format("%s;%s;%s", bin, lib, path);
+        environment.put("PATH", path);
+    }
 
+    /**
+     * 停止系统中全部 mserver5.exe 进程.
+     */
+    public void stopServer() {
+        logger.debug("停止服务：{}", this.exePath.getName());
+        killExe(this.exePath.getName());
+    }
+
+    private void killExe(String exeName) {
+        ProcessBuilder processBuilder = new ProcessBuilder("taskkill.exe", "/F", "/IM", exeName);
+        try {
+            Process proc = processBuilder.start();
+            try {
+                int ret = proc.waitFor();
+                logger.debug("taskkill {} ret: {}", exeName, ret);
+            } catch (InterruptedException e) {
+                logger.info("Kill exe interrupted!", e);
+            }
+        } catch (IOException e) {
+            logger.error("Kill exe fail!", e);
+        }
     }
 
     /**
@@ -84,6 +128,7 @@ public class MonetDBServer {
      * @return true 表示服务进程已经启动，正在运行；false 表示服务未运行。
      */
     public boolean isRunning(long timeout) throws TimeoutException {
+        MapiSocket socket = new MapiSocket();
         return false;
     }
 }
